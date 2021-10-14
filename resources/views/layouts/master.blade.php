704a0e67418d176592c8c2a52dd672276fef8224
Denis <gaz91.91@mail.ru>
2021-10-14 16:28:45 +0500

    Глобальное обновление сайта

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title') | Панель администратора</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendors/bootstrap-icons/bootstrap-icons.css') }}">
+  @yield('styles', '')
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+</head>
+
+<body>
+<div id="app">
+  <div id="sidebar" class="active">
+    @include('layouts.sidebar')
+  </div>
+  <div id="main">
+    <header class="mb-3">
+      <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+      </a>
+    </header>
+    @yield('content')
+    {{--            {% block content %}{% endblock %}--}}
+    {{--            <footer>--}}
+    {{--                <div class="footer clearfix mb-0 text-muted">--}}
+    {{--                    <div class="float-start">--}}
+    {{--                        <p>2021 &copy; Mazer</p>--}}
+    {{--                    </div>--}}
+    {{--                    <div class="float-end">--}}
+    {{--                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a--}}
+    {{--                                href="http://ahmadsaugi.com">A. Saugi</a></p>--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--            </footer>--}}
+  </div>
+</div>
+<script src="{{ asset('vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('vendors/bootstrap/bootstrap.bundle.min.js') }}"></script>
+@yield('scripts', '')
+<script src="{{ asset('js/mazer.js') }}"></script>
+</body>
+
+</html>
